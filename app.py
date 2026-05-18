49a3c78c70d7d83c40bd911a1b34c02fb35c8bea
sahajchandra <107821689+sahajchandra@users.noreply.github.com>
2026-05-18 13:52:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,12 @@ def extract():
         pdf_url = data.get('pdf_url')
         headers = {'User-Agent': 'Mozilla/5.0'}
         response = requests.get(pdf_url, timeout=30, headers=headers)
-        images = convert_from_bytes(response.content)
+        images = convert_from_bytes(
+            response.content,
+            dpi=150,
+            first_page=1,
+            last_page=2
+        )
         text = ''
         for image in images:
             text += pytesseract.image_to_string(image) + '\n'
